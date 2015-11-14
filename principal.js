a7dd943fbf3f8c143571bbed0e90b364473a23a8
Natássia Medeiros <natassiamsads@gmail.com>
2015-11-14 16:20:21 -0200

    Alternando textos do banner

---
+++ principal.js
@@ -0,0 +1,3 @@
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
+function trocaBanner() { bannerAtual = (bannerAtual +1 ) %2 ; document.querySelector(’h2#mensagem’).textContent = banners[bannerAtual]; }
+setInterval(trocaBanner, 1000);
